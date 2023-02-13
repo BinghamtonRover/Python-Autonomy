fc13bdbf929136a28926f2aeb561f9a1f4d1f9e1
Jack McMahon <60189032+jmcmah10@users.noreply.github.com>
2023-02-13 14:45:06 -0500

    Created class for Tank (#14)
    * Created class for Tank
    
    * Added more stuff
    
    * Apply suggestions from code review
    
    * Cleanup and fix errors
    
    ---------
    
    Co-authored-by: Levi Lesches <levilesches@gmail.com>

--- demo/demo.py
+++ demo/demo.py
@@ -1,22 +1,13 @@
 import time
 import sys
-import driver as camera
+import demo.driver as camera
 import socket
-import tank
+from demo.tank import Tank
+from demo.autonomy_drive import AutonomyRover
 
 def read_marker():
     # plan to return marker horizontal location on screen
     return camera.read_camera_for_marker()
-
-def send_right_speed(speed):
-    tank.set_right_velocity(speed)
-
-def send_left_speed(speed):
-    tank.set_left_velocity(speed)
-    
-def move_forward(speed):
-    send_left_speed(speed)
-    send_right_speed(speed)
 
 """
 def get_adjust_speed(search_speed, min_speed, target, width):
@@ -31,7 +22,7 @@ def get_adjust_speed(search_speed, min_speed, target, width):
     return adjust_speed
 """
 
-def main():
+def main(drive):
     #values to play around with
     search_speed = 6.1
     adjust_speed = 5.1
@@ -46,14 +37,12 @@ def main():
     wait_time = 0.01
 
     # initialization
-    tank.initialize_tank()
     camera.initialize_marker_detection()
     
     time.sleep(4)
 
     # yeah, that's right, I turn counter-clockwise
-    send_left_speed(-search_speed)
-    send_right_speed(search_speed)
+    drive.send_drive_data(search_speed, -1.0, 1.0)
 
     x_pos = None
     while True:
@@ -64,31 +53,27 @@ def main():
             if abs(diff) < target_range:
                 break
             elif diff < 0.0:
-                send_left_speed(-adjust_speed)
-                send_right_speed(adjust_speed)
+                drive.send_drive_data(adjust_speed, -1.0, 1.0)
             else:
-                send_left_speed(adjust_speed)
-                send_right_speed(-adjust_speed)
-            #adjust_speed = get_adjust_speed(search_speed, min_adjust_speed, middle_screen_value, screen_width)
-            #send_left_speed(-adjust_speed)
-            #send_right_speed(adjust_speed)
+                drive.send_drive_data(adjust_speed, 1.0, -1.0)
         if wait_in_loops:
             time.sleep(wait_time)
             
-    move_forward(0.0)
+    drive.send_drive_data(0.0, 0.0, 0.0)
     
-    time.sleep(0.5)
+    time.sleep(1.0)
 
-    move_forward(search_speed)
+    drive.send_drive_data(search_speed, 1.0, 1.0)
 
     time.sleep(move_forward_time)
 
-    move_forward(0.0)
+    drive.send_drive_data(0.0, 0.0, 0.0)
     
     camera.clean_up()
 
 if __name__ == "__main__":
+    drive = Tank()  # or Rover()
     try:
-        main()
+        main(drive)
     finally:
-        move_forward(0.0)
+        drive.send_drive_data(0.0, 0.0, 0.0)
