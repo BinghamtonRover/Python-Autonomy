50b8da6bdce248321c9e7b76a9ac9d5b33f57cda
Levi Lesches <levilesches@gmail.com>
2023-02-13 14:25:58 -0500

    Apply suggestions from code review

--- demo/autonomy_drive.py
+++ demo/autonomy_drive.py
@@ -1,9 +1,5 @@
 import proto_sender
-#import drive_pb2
-#import drive_pb3
-
-
-
+from lib.network.generated.Protobuf.drive_pb2 import *
 ''' 
 Unanswered questions:
 	What proto files encapsulate the compass, gyroscope, and gps data?
@@ -13,17 +9,14 @@ THROTTLE_DEFAULT = 0
 LEFT_DEFAULT = 0
 RIGHT_DEFAULT = 0
 
-DRIVE_COMMAND_NAME = "Drive"
+DRIVE_COMMAND_NAME = "DriveCommand"
 
 
 #potential class for sending data from AutonomyPi to MainPi
 class AutonomyRover:
 
 	def __init__(self, ip, port, buffer_size):
-		set_receiving_pi(ip, port, buffer_size)
-
-	def set_receiving_pi(self, ip, port, buffer_size):
-		self.messenger = ProtoSender(ip,port,buffer_size)
+		self.messenger = ProtoSender(IP, port, buffer_size)
 	
 	def send_drive_data(self, throttle = THROTTLE_DEFAULT, left = LEFT_DEFAULT, right = RIGHT_DEFAULT):
 		drive_message = drive_pb2.DriveCommand()
@@ -37,15 +30,11 @@ class AutonomyRover:
 		drive_message.throttle = throttle
 		self.messenger.send_proto(drive_message, DRIVE_COMMAND_NAME)
 
-	def adjust_right(self, right = RIGHT_DEFAULT):
-		drive_message = drive_pb2.DriveCommand()
-		drive_message.right = right
-		self.messenger.send_proto(drive_message, DRIVE_COMMAND_NAME)
-
-	def adjust_left(self, left = LEFT_DEFAULT):
-		drive_message = drive_pb2.DriveCommand()
-		drive_message.left = left
-		self.messenger.send_proto(drive_message, DRIVE_COMMAND_NAME)
+	def adjust_speed(self, left, right):
+		command = DriveCommand()
+		command.right = right
+		command.left = left
+		self.messenger.send_proto(command, DRIVE_COMMAND_NAME)
 
 	def send_compass_data(self, direction):
 		pass
