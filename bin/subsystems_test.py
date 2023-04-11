1e893ad9c0be73b020d5d245b8971455712985c8
Levi Lesches <levilesches@gmail.com>
2023-04-11 19:08:29 -0400

    Updated `subsystems_test` to use `Drive` instead of making its own commands

--- bin/subsystems_test.py
+++ bin/subsystems_test.py
@@ -2,20 +2,20 @@ import time
 
 from network import ProtoSocket, Device
 from network.src.generated.Protobuf.drive_pb2 import DriveCommand
+from lib.drive import Drive
 
-client = ProtoSocket(port=8000, device=Device.DASHBOARD, destination=("127.0.0.1", 8001))
+socket = ProtoSocket(port=8000, device=Device.DASHBOARD, destination=("127.0.0.1", 8001))
+drive = Drive(socket)
 
 def drive_forward(): 
-	client.send_message(DriveCommand(set_left=True, left=1, set_right=True, right=1, set_throttle=True, throttle=1))
+	drive.set_speeds(left=1.0, right=1.0)
 
 def drive_backward(): 
-	client.send_message(DriveCommand(set_left=True, left=-1, set_right=True, right=-1, set_throttle=True, throttle=1))
-
-def stop(): 
-	client.send_message(DriveCommand(set_throttle=True, throttle=0))
+	drive.set_speeds(left=-1.0, right=-1.0)
 
 if __name__ == "__main__": 
 	try: 
+		drive.set_throttle(1.0)
 		while True: 
 			drive_forward()
 			time.sleep(2)
@@ -23,5 +23,5 @@ if __name__ == "__main__":
 			time.sleep(2)
 	except KeyboardInterrupt: pass
 	finally: 
-		stop()
-		client.close()
+		drive.set_throttle(0)
+		socket.close()
