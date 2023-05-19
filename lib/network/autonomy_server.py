161fa8df6006d885c7c75f5da37f656f061a94b5
Levi Lesches <levilesches@gmail.com>
2023-05-19 05:33:24 -0400

    Added `AutonomyServer`, to handle autonomy commands and data

---
+++ lib/network/autonomy_server.py
@@ -0,0 +1,42 @@
+from network import *
+from network.generated import *
+
+from lib.concurrency import AutonomyThread
+
+class AutonomyServer(ProtoSocket): 
+	def __init__(self, port, collection): 
+		super().__init__(port=port, device=Device.AUTONOMY)
+		self.collection = collection
+
+	def on_disconnect(self): 
+		super().on_disconnect()
+		self.disable_autonomy()
+
+	def update_settings(self, settings): 
+		super().update_settings(settings)
+		if settings.status in [RoverStatus.IDLE, RoverStatus.MANUAL]: 
+			self.disable_autonomy()
+		elif settings.status == RoverStatus.AUTONOMOUS: 
+			print("Autonomy enabled")
+			pass  # don't do anything until AutonomyCommand is received
+
+	def on_message(self, wrapper): 
+		if wrapper.name == "AutonomyCommand": 
+			settings = UpdateSetting.FromString(self.settings)
+			if settings.status != RoverStatus.AUTONOMOUS: 
+				print("Ignoring AutonomyCommands until we are in autonomous mode")
+				return
+			command = AutonomyCommand.FromString(wrapper.data)
+			self.send_message(command)
+			print(f"Received autonomy command: {command}")
+			self.enable_autonomy(command)
+
+	def enable_autonomy(self, command): 
+		print("Running autonomy...")
+		self.disable_autonomy()
+		self.collection.autonomy.command = command
+		self.collection.autonomy.start()
+
+	def disable_autonomy(self): 
+		self.collection.autonomy.close()
+		self.collection.autonomy = AutonomyThread(collection=self.collection)
