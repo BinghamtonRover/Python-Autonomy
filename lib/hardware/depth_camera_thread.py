683b533720e44d8e36044199d8f3a24f2309bae6
Levi Lesches <levilesches@gmail.com>
2023-05-31 20:19:50 -0600

    Spaces

--- lib/hardware/depth_camera_thread.py
+++ lib/hardware/depth_camera_thread.py
@@ -7,45 +7,45 @@ from network import *
 from network.generated import *
 
 class DepthCameraThread(threading.Thread):
-	def __init__(self, collection, test=False):
-		print("[Info] Initializing Depth Camera...")
-		super().__init__()
-		self.keep_alive = True
-		#if test or sys.platform != "linux":
-		#	self.session = MockGps()
-		#else: 
-		#	self.session = gps.gps("localhost", "2947")
-		self.collection = collection
-		self.depth_frame = None
-		self.color_frame = None
-		self.init_realsense()
-		self.read_frames()
+    def __init__(self, collection, test=False):
+        print("[Info] Initializing Depth Camera...")
+        super().__init__()
+        self.keep_alive = True
+        #if test or sys.platform != "linux":
+        #   self.session = MockGps()
+        #else: 
+        #   self.session = gps.gps("localhost", "2947")
+        self.collection = collection
+        self.depth_frame = None
+        self.color_frame = None
+        self.init_realsense()
+        self.read_frames()
 
-	def close(self): 
-		self.keep_alive = False
+    def close(self): 
+        self.keep_alive = False
 
-	# TODO: Remove in favor of directly accessing self.coordinates
-	def read_depth_frame(self):
-		return self.depth_frame
-	
-	def read_color_frame(self):
-		return self.color_frame
+    # TODO: Remove in favor of directly accessing self.coordinates
+    def read_depth_frame(self):
+        return self.depth_frame
+    
+    def read_color_frame(self):
+        return self.color_frame
 
     def set_orientation(self):
         self.collection.drive.set_camera_tilt(self.collection.imu.get_orientation()[0] + 90.0)
 
-	def read_frames(self):
+    def read_frames(self):
         frames = self.pipeline.wait_for_frames()
         self.depth_frame = frames.get_depth_frame()
         self.color_frame = frames.get_color_frame()
-		self.collection.video.send_frame(frame=self.color_frame, name=CameraName.ROVER_FRONT)
-		self.collection.video.send_frame(frame=cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET), name=CameraName.AUTONOMY_DEPTH)
+        self.collection.video.send_frame(frame=self.color_frame, name=CameraName.ROVER_FRONT)
+        self.collection.video.send_frame(frame=cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET), name=CameraName.AUTONOMY_DEPTH)
 
-	def run(self):
-		while self.keep_alive:
+    def run(self):
+        while self.keep_alive:
             # self.set_orientation()
-			self.read_frames()
-			
+            self.read_frames()
+            
     def init_realsense(self):
         # Load proper settings
         self.load_high_density_settings()
@@ -58,7 +58,7 @@ class DepthCameraThread(threading.Thread):
         # Start streaming
         self.pipeline.start(self.config)
 
-	    def _find_device_that_supports_advanced_mode(self) :
+        def _find_device_that_supports_advanced_mode(self) :
         ctx = rs.context()
         ds5_dev = rs.device()
         devices = ctx.query_devices()
@@ -67,7 +67,7 @@ class DepthCameraThread(threading.Thread):
         for dev in devices:
             if dev.supports(rs.camera_info.product_id) and str(dev.get_info(rs.camera_info.product_id)) in DS5_PRODUCT_IDS:
                 print(rs.camera_info.name)
-               	if dev.supports(rs.camera_info.name):
+                if dev.supports(rs.camera_info.name):
                     print("Found device that supports advanced mode:", dev.get_info(rs.camera_info.name))
                 return dev
         raise Exception("No device that supports advanced mode was found")
